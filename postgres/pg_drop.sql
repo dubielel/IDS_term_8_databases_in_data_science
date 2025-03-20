235dadff28e7c20ae3a789a965a0f67528732929
dubielel <mdubielel@gmail.com>
2025-03-20 19:17:26 +0100

    Working postgres

---
+++ postgres/pg_drop.sql
@@ -0,0 +1,36 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+
+---
+--- drop tables
+---
+
+
+DROP TABLE IF EXISTS customercustomerdemo;
+DROP TABLE IF EXISTS customerdemographics;
+DROP TABLE IF EXISTS employeeterritories;
+DROP TABLE IF EXISTS orderdetails;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS shippers;
+DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS territories;
+DROP TABLE IF EXISTS usstates;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS region;
+DROP TABLE IF EXISTS employees;
